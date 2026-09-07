742192443b104314adb83d3b7a38a570816e4ea7
nirholas <claudescammer@outlook.com>
2026-09-07 20:36:21 +0000

    chore: regenerate from the HookForge catalogue

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -36,6 +36,7 @@ import {RatchetFloorHook} from "src/hooks/RatchetFloorHook.sol";
 contract DeployRatchetFloor is Script {
     uint160 internal constant FLAGS = uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.AFTER_SWAP_FLAG);
 
+
     function run() external {
         IPoolManager manager = Chains.poolManager(block.chainid);
         require(address(manager) != address(0), "no Uniswap v4 PoolManager known for this chain");
